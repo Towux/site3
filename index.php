e30da11ee68ea943c0bccf12975ee351c0d674d6
Towa <70810089+Towux@users.noreply.github.com>
2023-05-03 20:56:58 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>сосать хуи</title>
+</head>
+<body>
+    <h1>че то типо да</h1>
+    <br>
+    <h2>че то типо нет</h2>
+    <ul>
+        <li>иоч</li>
+        <li>ичо</li>
+        <li>оич</li>
+        <li>очи</li>
+        <li>чио</li>
+        <li>чои</li>
+    </ul>
+    <?php
+    function () {
+        echo 'и тут мы такие типо пон и такие непон
+        кароч здесь делаем php манипуляции которые скрыты от глаз';
+    }
+    ?>
+</body>
+</html>
